a08810ef8a47c3ae438656f509516ed858ff91a9
akshar <aksharp99@gmail.com>
2016-09-26 11:33:12 -0600

    randomized friend list

---
+++ js/mainCtrl.js
@@ -0,0 +1,17 @@
+angular.module('friendsList').controller('mainCtrl', function($scope) {
+  $scope.name = "Akshar";
+  $scope.listOfFriends = ["Ken","Nick","Tunde","Rahul","Minh","Phu","Alex"];
+
+  $scope.x = angular.element(document.querySelector('#special'));
+  $scope.test = $scope.x;
+
+  $scope.test.on('click', function() {
+
+    var i = Math.floor(Math.random() * $scope.listOfFriends.length);
+
+    $scope.alldesefriends = $scope.listOfFriends[i];
+
+
+  })
+
+})
